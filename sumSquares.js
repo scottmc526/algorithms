b56339fe4568a4505cb365888d9831f5c8aaafb9
Scott Christensen <scottmc526@gmail.com>
2016-04-01 13:58:44 -0600

    101 dalmatians

--- sumSquares.js
+++ sumSquares.js
@@ -1,5 +1,5 @@
 //returns an array of length equal to num
-function newStuff (num){
+function createArray (num){
  var arr = []
  for (var i = 1; i <= num; i++) {
    arr.push(i)
@@ -11,8 +11,8 @@ function newStuff (num){
 //squares each element in array and returns sum of those squares
 function sumSquares(n) {
   var total = 0;
-  newStuff(n).reduce(function (a, a) {total += a * a}, 0)
+  createArray(n).reduce(function (x, x) {total += x * x}, 0)
   return total;
 }
 
-console.log(sumSquares(100000000));
+console.log(sumSquares(40));
