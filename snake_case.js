21c28d3431724045f26097e894a2ae67b7999e6d
Scott Christensen <scottmc526@gmail.com>
2016-05-03 17:15:43 -0600

    syntax

--- snake_case.js
+++ snake_case.js
@@ -2,11 +2,11 @@
 //takes string in snake_case and converts to camelCase
 function camelConverter(string) {
  var capital = '';
- var camel = string.split('_')
- var toCapitalize = camel.slice(1, camel.length)
+ var camel = string.split('_');
+ var toCapitalize = camel.slice(1, camel.length);
  toCapitalize.reduce(function(prev, curr) {
      capital += curr.charAt(0).toUpperCase() + curr.slice(1);
-   }, 0)
+   }, 0);
   return camel[0] + capital
 }
 
