4d44969ddd5a288a9efa88311c4f19e080933f46
Scott Christensen <scottmc526@gmail.com>
2016-05-03 17:10:39 -0600

    param

--- snake_case.js
+++ snake_case.js
@@ -1,8 +1,8 @@
 
 //takes string in snake_case and converts to camelCase
-function camelConverter(functionName) {
+function camelConverter(string) {
  var capital = '';
- var camel = functionName.split('_')
+ var camel = string.split('_')
  var toCapitalize = camel.slice(1, camel.length)
  toCapitalize.reduce(function(prev, curr) {
      capital += curr.charAt(0).toUpperCase() + curr.slice(1);
