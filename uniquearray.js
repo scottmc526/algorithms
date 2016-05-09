e7dac6b90723c485833027b7964522147eb19863
Scott Christensen <scottmc526@gmail.com>
2016-05-09 14:33:43 -0600

    unique array problem

---
+++ uniquearray.js
@@ -0,0 +1,24 @@
+
+//make an array of arguments
+function makeArray (x) {
+  var arr = [];
+  for (var i = 0; i < x.length; i++) {
+    arr.push(x[i]);
+   }
+   return arr;
+}
+
+//make single array out of array of arrays and remove dupes
+function uniteUnique() {
+ var arrays = makeArray(arguments);;
+ var merged = [].concat.apply([], arrays);
+ uniqueArray = merged.filter(function(item, pos) {
+    return merged.indexOf(item) == pos;
+  })
+
+  return uniqueArray;
+
+
+}
+
+console.log(uniteUnique([1,2,4,5], [1,3,5], ['a', 'b']));
