b3c83ab0ace430bd237f3959a7e1b54380bc2957
Scott Christensen <scottmc526@gmail.com>
2016-05-09 14:31:23 -0600

    snake refacor

--- snake_case.js
+++ snake_case.js
@@ -1,13 +1,13 @@
 
 //takes string in snake_case and converts to camelCase
 function camelConverter(string) {
- var capital = '';
+ var Case = '';
  var camel = string.split('_');
  var toCapitalize = camel.slice(1, camel.length);
  toCapitalize.reduce(function(prev, curr) {
-     capital += curr.charAt(0).toUpperCase() + curr.slice(1);
+     Case += curr.charAt(0).toUpperCase() + curr.slice(1);
    }, 0);
-  return camel[0] + capital
+  return camel[0] + Case
 }
 
 console.log(camelConverter('hello_world'));
