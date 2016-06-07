e68c22080139927560cd42b350c9c0f62fe046a9
Scott Christensen <scottmc526@gmail.com>
2016-06-06 20:00:44 -0600

    pyramid problem

---
+++ flowhub.js
@@ -0,0 +1,18 @@
+function pyramid (height) {
+
+  for (var i = 1; i <= height; i++) {
+    var row = '';
+
+    for (var j = 1; j <= (height - i); j++) {
+      row += ' ';
+    }
+
+    for (var k = 1; k <= i*2-1; k++) {
+      row += '#';
+    }
+
+    console.log(row);
+  }
+}
+
+createHalfPyramid(3);
