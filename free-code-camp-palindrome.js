9af317d059dcd35f104476c4206615c6e77687ea
Scott Christensen <scottmc526@gmail.com>
2016-12-24 19:35:34 -0700

    palindrome refactor

--- free-code-camp-palindrome.js
+++ free-code-camp-palindrome.js
@@ -1,11 +1,10 @@
-//function accepts a string and removes all non aplha numeric characters and spaces and then checks if the string is a palindrome. 
+//function accepts a string and removes all non aplha numeric characters and spaces and then checks if the string is a palindrome.
 
 function palindrome(str) {
   var regex = /([A-Za-z0-9])+/g;
-  var nonAlphaCharsRemoved = str.match(regex).join('').toLowerCase();
-  var spacesRemoved = nonAlphaCharsRemoved.replace(/ /g,'');
-  var reversed = spacesRemoved.split('').reverse().join('');
-  return spacesRemoved === reversed;
+  var correctedString = str.match(regex).join('').toLowerCase().replace(/ /g, '');
+  var reversed = correctedString.split('').reverse().join('');
+  return correctedString === reversed;
 }
 
 palindrome("A man, a plan, a canal. Panama")
