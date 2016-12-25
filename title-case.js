9620ab5f4030e4cdcde8eb94b8b1d7125ba290ee
Scott Christensen <scottmc526@gmail.com>
2016-12-25 12:30:01 -0700

    title case refactor

--- title-case.js
+++ title-case.js
@@ -1,15 +1,10 @@
 //funciton takes a string and capitalizes the first letter of each word and returns the rest of the world in lower case
 
 function titleCase (str){
-  var word = str.split(' ');
-  var newWordArray = [];
-  var firstLetter;
-
-  for (var i = 0; i < arr.length; i++) {
-    firstLetter = word[i].slice(0,1).toUpperCase();
-    newWordArray.push(firstLetter + word[i].slice(1,word[i].length).toLowerCase())
-  }
-  return newWordArray.join(' ');
+  var words = str.split(' ');
+  return newWordArray = words.map((word) => {
+    return word.slice(0,1).toUpperCase() + word.slice(1, word.length).toLowerCase();
+  }).join(' ')
 }
 
 
