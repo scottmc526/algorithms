154a68842a25e071de4c3ab514c8c47a72f5d828
Scott Christensen <scottmc526@gmail.com>
2016-12-26 14:02:22 -0700

    consolidated small functions

--- repeat-string-and-confirm-ending.js
+++ repeat-string-and-confirm-ending.js
@@ -1,3 +1,10 @@
+//function that repeats a string a certian amount of times as long as num is positive
+function repeatStringNumTimes(str, num) {
+  return num < 0 ? '' : str.repeat(num);
+}
+
+repeatStringNumTimes("abc", 3);
+
 //function returns a boolean which determines whether or not a string ends with the target argument
 
 function confirmEnding(str, target) {
--- repeat-string-num-times.js
+++
@@ -1,6 +0,0 @@
-//function that repeats a string a certian amount of times as long as num is positive
-function repeatStringNumTimes(str, num) {
-  return num < 0 ? '' : str.repeat(num);
-}
-
-repeatStringNumTimes("abc", 3);
